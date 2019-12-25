a40697028376c0f8dfb60ed00b3075eea97cfbb5
Oliver Rew <oliver.rew@rigado.com>
2019-12-25 13:43:11 -0500

    cleaned up some pins

--- owen-busy-box.ino
+++ owen-busy-box.ino
@@ -6,14 +6,22 @@
 #define LED2 10
 #define BUZZ 11
 #define LED3 12
+#define RGB_DELAY 10
+
+struct pin {
+  uint8_t pin;
+  bool active; //active level
+};
 
 //inputs
-#define START 8
-#define STOP 7 //ACTIVE HIGH
-#define CENTER 2
-#define ZBUT 13
-#define POT A0
-#define RGB_DELAY 10
+pin START = {A2, LOW};
+pin STOP = {A3, HIGH};
+pin ZBUT = {13, LOW};
+pin CENTER = {2, LOW};
+pin START_ROUND = {8, LOW};
+pin STOP_ROUND = {7, HIGH};
+pin POT = {A0}; // analog input pin
+
 uint8_t redVal;
 uint8_t blueVal;
 uint8_t greenVal;
@@ -56,11 +64,12 @@ void setup() {
   digitalWrite(LED2, HIGH);
 
   //init inputs
-  pinMode(START, INPUT_PULLUP);
-  pinMode(STOP, INPUT_PULLUP); //ACTIVE HIGH
-  pinMode(CENTER, INPUT_PULLUP);
-  pinMode(ZBUT, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(CENTER), intHandler, FALLING); //interrupt on center button
+  pinMode(START_ROUND.pin, INPUT_PULLUP);
+  pinMode(STOP_ROUND.pin, INPUT_PULLUP); //ACTIVE HIGH
+  pinMode(CENTER.pin, INPUT_PULLUP);
+  pinMode(ZBUT.pin, INPUT_PULLUP);
+  //pinMode(START.pin, INPUT_PULLUP); //TODO
+  attachInterrupt(digitalPinToInterrupt(CENTER.pin), intHandler, (CENTER.pin == LOW ? FALLING : RISING)); //interrupt on center button
 
   //other
   Serial.begin(115200);
@@ -81,38 +90,59 @@ void setup() {
   ping();
 }
 
+bool active(pin in)
+{
+  if(digitalRead(in.pin) == in.active)
+    return true;
+  else
+    return false;
+}
+
 void loop()
 {
+//  if (active(START))
+//  {
+//    digitalWrite(LED3, HIGH);
+//    Serial.println("pressed");
+//  }
+//  else
+//  {
+//    digitalWrite(LED3, LOW);
+//    Serial.println("not pressed");
+//  }
+//
+//  return;
+
   if (prevState != state)
   {
     prevState = state;
     ping();
   }
-  if (!digitalRead(ZBUT))
+  if (!digitalRead(ZBUT.pin))
   {
     playSong1();
   }
 
-  if (!digitalRead(START))
+  if (!digitalRead(START_ROUND.pin))
   {
     for (int i = 140 ; i < 2000 ; i++)
     {
-      int offset = map(analogRead(POT), 0, 1023, -100, 500);
+      int offset = map(analogRead(POT.pin), 0, 1023, -100, 500);
       tone(BUZZ, i + offset);
-      delayMicroseconds(analogRead(POT)/2);
+      delayMicroseconds(analogRead(POT.pin)/2);
     }
     for (int i = 2000 ; i > 140 ; i--)
     {
-      int offset = map(analogRead(POT), 0, 1023, -100, 500);
+      int offset = map(analogRead(POT.pin), 0, 1023, -100, 500);
       tone(BUZZ, i + offset);
-      delayMicroseconds(analogRead(POT)/2);
+      delayMicroseconds(analogRead(POT.pin)/2);
     }
     noTone(BUZZ);
   }
 
-  if (digitalRead(STOP))
+  if (digitalRead(STOP_ROUND.pin))
   {
-    int offset = map(analogRead(POT), 0, 1023, -20, 100);
+    int offset = map(analogRead(POT.pin), 0, 1023, -20, 100);
     tone(BUZZ, 100);
     delay(50 + offset);
     tone(BUZZ, 2000);
@@ -154,14 +184,14 @@ void playSong1()
     // to calculate the note duration, take one second divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
     int noteDuration = 1000 / melody1Notes[thisNote];
-    int noteScale = map(analogRead(POT), 0, 1023, 80, 200);
+    int noteScale = map(analogRead(POT.pin), 0, 1023, 80, 200);
     float noteFloat = float(noteScale) / float(100);
     tone(BUZZ, melody1[thisNote]*noteFloat, noteDuration);
 
     // to distinguish the notes, set a minimum time between them.
     // the note's duration + 30% seems to work well:
     int pauseBetweenNotes = noteDuration * 1.30;
-    int potScale = map(analogRead(POT), 0, 1023, -100, 200);
+    int potScale = map(analogRead(POT.pin), 0, 1023, -100, 200);
     delay(pauseBetweenNotes + potScale);
     // stop the tone playing:
     noTone(BUZZ);
@@ -211,7 +241,7 @@ void pulse()
           return;
         }
         analogWrite(LEDS[startState], i);
-        delayMicroseconds(2 * analogRead(POT));
+        delayMicroseconds(2 * analogRead(POT.pin));
       }
       for (int i = 255; i > 0 ; i--)
       {
@@ -221,7 +251,7 @@ void pulse()
           return;
         }
         analogWrite(LEDS[startState], i);
-        delayMicroseconds(2 * analogRead(POT));
+        delayMicroseconds(2 * analogRead(POT.pin));
       }
       clearLEDs();
       break;
@@ -230,13 +260,13 @@ void pulse()
 
 bool checkPins()
 {
-  if (!digitalRead(ZBUT))
+  if (!digitalRead(ZBUT.pin))
     return 1;
 
-  if (!digitalRead(START))
+  if (!digitalRead(START_ROUND.pin))
     return 1;
 
-  if (digitalRead(STOP))
+  if (digitalRead(STOP_ROUND.pin))
     return 1;
   return 0;
 }
